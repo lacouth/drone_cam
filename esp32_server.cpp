d696d6f0b22192fc28588fb551c6278d4a218726
Patric Lacouth <lacouth@gmail.com>
2023-09-21 17:39:20 -0300

    adiciona esp32_server.cpp e opencv.py

---
+++ esp32_server.cpp
@@ -0,0 +1,80 @@
+#include <esp32cam.h>
+#include <WebServer.h>
+#include <WiFi.h>
+
+const char* WIFI_SSID = "";
+const char* WIFI_PASS = "";
+
+WebServer server(80);
+
+static auto hiRes = esp32cam::Resolution::find(800, 600);
+
+
+void serveJpg()
+{
+  auto frame = esp32cam::capture();
+  if (frame == nullptr) {
+    Serial.println("CAPTURE FAIL");
+    server.send(503, "", "");
+    return;
+  }
+  Serial.printf("CAPTURE OK %dx%d %db\n", frame->getWidth(), frame->getHeight(),
+                static_cast<int>(frame->size()));
+
+  server.setContentLength(frame->size());
+  server.send(200, "image/jpeg");
+  WiFiClient client = server.client();
+  frame->writeTo(client);
+}
+
+void handleJpgHi()
+{
+  if (!esp32cam::Camera.changeResolution(hiRes)) {
+    Serial.println("SET-HI-RES FAIL");
+  }
+  serveJpg();
+}
+
+void handleJpg()
+{
+  server.sendHeader("Location", "/cam.jpg");
+  server.send(302, "", "");
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  {
+    using namespace esp32cam;
+    Config cfg;
+    cfg.setPins(pins::AiThinker);
+    cfg.setResolution(hiRes);
+    cfg.setBufferCount(2);
+    cfg.setJpeg(80);
+
+    bool ok = Camera.begin(cfg);
+    Serial.println(ok ? "CAMERA OK" : "CAMERA FAIL");
+  }
+
+  WiFi.persistent(false);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+
+  Serial.print("http://");
+  Serial.println(WiFi.localIP());
+  Serial.println("  /cam.jpg");
+
+  server.on("/cam.jpg", handleJpgHi);
+  
+  server.begin();
+}
+
+void loop()
+{
+  server.handleClient();
+}
